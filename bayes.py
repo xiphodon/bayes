375c565305cb87f21bdfdf4b90f959d3d5437cfd
xiphodon <827789895@qq.com>
2016-12-16 19:16:10 +0800

    建立朴素贝叶斯分类项目实例

---
+++ bayes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2016/12/16 19:14
+# @Author  : GuoChang
+# @Site    : https://github.com/xiphodon
+# @File    : bayes.py
+# @Software: PyCharm
+
+# 朴素贝叶斯分类
